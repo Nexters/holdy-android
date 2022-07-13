a2627aaf55a49cc4b4fdb124ef3e7d5f5f8b2895
DESKTOP-SGS6ULA\evergreen <tkdfhr7497@naver.com>
2022-07-13 23:56:39 +0900

    feat : add BaseViewModel

---
+++ app/src/main/java/team/nexters/semonemo/base/BaseViewModel.kt
@@ -0,0 +1,16 @@
+package team.nexters.semonemo.base
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.launch
+
+abstract class BaseViewModel : ViewModel() {
+    private val _exceptionChannel = Channel<Throwable>()
+    val exceptionFlow = _exceptionChannel.receiveAsFlow()
+
+    open fun emitException(exception: Throwable) = viewModelScope.launch {
+        _exceptionChannel.send(exception)
+    }
+}
