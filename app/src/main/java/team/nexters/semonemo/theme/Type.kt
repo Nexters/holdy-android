b1b27204b6e4d839245fddebaf26042a03496680
DESKTOP-SGS6ULA\evergreen <tkdfhr7497@naver.com>
2022-07-20 16:09:35 +0900

    feat : 임시로 기본 textstyle 설정

--- app/src/main/java/team/nexters/semonemo/theme/Type.kt
+++ app/src/main/java/team/nexters/semonemo/theme/Type.kt
@@ -11,9 +11,9 @@ val Typography = Typography(
     bodyLarge = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
+        fontSize = 14.sp,
+        lineHeight = (19.6).sp,
+        letterSpacing = (-2).sp
     )
     /* Other default text styles to override
     titleLarge = TextStyle(
