789e82e59b4d0644381815e6aafc029e52d6beb0
DESKTOP-SGS6ULA\evergreen <tkdfhr7497@naver.com>
2022-07-21 11:56:53 +0900

    test : repository, usecase 테스트 코드 및 더미 파일

---
+++ domain/src/test/java/team/nexters/domain/FetchMoimListUseCaseTest.kt
@@ -0,0 +1,32 @@
+package team.nexters.domain
+
+import io.mockk.coEvery
+import io.mockk.impl.annotations.MockK
+import io.mockk.junit5.MockKExtension
+import kotlinx.coroutines.test.runTest
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import team.nexters.domain.moim.usecase.FetchMoimListUseCase
+
+@ExtendWith(MockKExtension::class)
+class FetchMoimListUseCaseTest {
+
+    @MockK
+    private lateinit var fetchMoimListUseCase: FetchMoimListUseCase
+
+    @BeforeEach
+    fun init() {
+        coEvery {
+            fetchMoimListUseCase()
+        } returns Result.success(emptyList())
+    }
+
+    @DisplayName("test success")
+    @Test
+    fun `Usecase return empty list`() = runTest {
+        val result = fetchMoimListUseCase()
+        assert(result.isSuccess)
+    }
+}
