89c1c4a08a32305dc6a04a6c546d034b2760de4a
DESKTOP-SGS6ULA\evergreen <tkdfhr7497@naver.com>
2022-07-23 16:54:18 +0900

    feat : user api 작성

--- data/src/main/java/team/nexters/data/user/api/UserApi.kt
+++ data/src/main/java/team/nexters/data/user/api/UserApi.kt
@@ -1,4 +1,13 @@
 package team.nexters.data.user.api
 
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.POST
+import team.nexters.data.user.model.LoginResponse
+
 interface UserApi {
+    @POST("api/login")
+    suspend fun login(
+        @Body authKey: String
+    ): Response<LoginResponse>
 }
