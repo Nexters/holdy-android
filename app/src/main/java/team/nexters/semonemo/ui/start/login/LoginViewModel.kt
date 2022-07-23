cee9bcfcc876d4e99047aaeb958f3e9487b53545
DESKTOP-SGS6ULA\evergreen <tkdfhr7497@naver.com>
2022-07-23 16:55:06 +0900

    feat : ViewModel에 usecase 주입

--- app/src/main/java/team/nexters/semonemo/ui/start/login/LoginViewModel.kt
+++ app/src/main/java/team/nexters/semonemo/ui/start/login/LoginViewModel.kt
@@ -1,10 +1,16 @@
 package team.nexters.semonemo.ui.start.login
 
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.asSharedFlow
+import team.nexters.domain.user.usecase.LoginUseCase
 import team.nexters.semonemo.base.BaseViewModel
+import javax.inject.Inject
 
-class LoginViewModel : BaseViewModel() {
+@HiltViewModel
+class LoginViewModel @Inject constructor(
+    private val loginUseCase: LoginUseCase
+) : BaseViewModel() {
     private val _eventFlow = MutableSharedFlow<LoginEvent>(extraBufferCapacity = 1)
     val eventFlow = _eventFlow.asSharedFlow()
 
