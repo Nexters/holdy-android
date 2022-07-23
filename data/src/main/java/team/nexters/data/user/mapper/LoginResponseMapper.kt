05ee071c204d3990a045d79075920608804cfe27
DESKTOP-SGS6ULA\evergreen <tkdfhr7497@naver.com>
2022-07-23 16:54:35 +0900

    feat : mapper, repository 구현체 작성

--- data/src/main/java/team/nexters/data/user/mapper/LoginResponseMapper.kt
+++ data/src/main/java/team/nexters/data/user/mapper/LoginResponseMapper.kt
@@ -1,2 +1,13 @@
 package team.nexters.data.user.mapper
 
+import team.nexters.data.user.model.LoginResponse
+import team.nexters.domain.user.model.LoginModel
+
+internal fun LoginResponse.toDomain() = LoginModel(
+    result = result,
+    loginUser = loginUser?.toDomain()
+)
+
+private fun LoginResponse.LoginUser.toDomain() = LoginModel.LoginUser(
+    nickname = nickname
+)
--- data/src/main/java/team/nexters/data/user/repository/UserRepositoryImpl.kt
+++ data/src/main/java/team/nexters/data/user/repository/UserRepositoryImpl.kt
@@ -1,4 +1,21 @@
 package team.nexters.data.user.repository
 
-class UserRepositoryImpl {
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.withContext
+import team.nexters.data.di.IoDispatcher
+import team.nexters.data.extension.apiCall
+import team.nexters.data.user.api.UserApi
+import team.nexters.data.user.mapper.toDomain
+import team.nexters.domain.user.model.LoginModel
+import team.nexters.domain.user.repository.UserRepository
+import javax.inject.Inject
+
+class UserRepositoryImpl @Inject constructor(
+    private val userApi: UserApi,
+    @IoDispatcher private val dispatcher: CoroutineDispatcher
+) : UserRepository {
+    override suspend fun login(authKey: String): LoginModel = withContext(dispatcher) {
+        userApi.login(authKey).apiCall().toDomain()
+    }
+
 }
