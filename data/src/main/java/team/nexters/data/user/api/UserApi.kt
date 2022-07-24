d33ad1f2da031913f692c55ea16e7ab4756e4910
DESKTOP-SGS6ULA\evergreen <tkdfhr7497@naver.com>
2022-07-25 00:02:10 +0900

    feature : login api 추가

--- data/src/main/java/team/nexters/data/user/api/UserApi.kt
+++ data/src/main/java/team/nexters/data/user/api/UserApi.kt
@@ -3,11 +3,12 @@ package team.nexters.data.user.api
 import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.POST
-import team.nexters.data.user.model.LoginResponse
+import team.nexters.data.user.model.request.LoginRequest
+import team.nexters.data.user.model.response.LoginResponse
 
 interface UserApi {
-    @POST("api/login")
+    @POST("/api/login")
     suspend fun login(
-        @Body authKey: String
+        @Body loginRequestModel: LoginRequest
     ): Response<LoginResponse>
 }
