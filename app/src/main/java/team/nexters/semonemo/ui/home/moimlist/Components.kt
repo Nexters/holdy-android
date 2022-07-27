8021ac58368796db96975d18f80a217791ff2f33
DESKTOP-SGS6ULA\evergreen <tkdfhr7497@naver.com>
2022-07-27 23:05:42 +0900

    style : 패키지 변경

---
+++ app/src/main/java/team/nexters/semonemo/ui/home/moimlist/Components.kt
@@ -0,0 +1,20 @@
+package team.nexters.semonemo.ui.home.moimlist
+
+import androidx.compose.runtime.Composable
+
+@Composable
+internal fun TopBar(
+    title: String,
+){
+
+}
+
+@Composable
+internal fun TimeLine(){
+
+}
+
+@Composable
+internal fun MoimListColumn(){
+
+}
