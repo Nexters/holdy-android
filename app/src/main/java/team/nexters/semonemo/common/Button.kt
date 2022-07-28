9ec2451f152bdeac69f80500625ec5bd8cc523d2
DESKTOP-SGS6ULA\evergreen <tkdfhr7497@naver.com>
2022-07-29 00:43:32 +0900

    refactor : Button Shape custom 가능하도록 변경

--- app/src/main/java/team/nexters/semonemo/common/Button.kt
+++ app/src/main/java/team/nexters/semonemo/common/Button.kt
@@ -1,25 +1,25 @@
 package team.nexters.semonemo.common
 
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.graphics.Shape
 
 @Composable
 internal fun Button(
     modifier: Modifier = Modifier,
     onClick: () -> Unit,
     colors: ButtonColors = ButtonDefaults.buttonColors(),
+    shape: Shape,
     text: String
 ) {
     Button(
         modifier = modifier,
         onClick = { onClick() },
-        shape = RoundedCornerShape(8.dp),
+        shape = shape,
         colors = colors
     ) {
         Text(
