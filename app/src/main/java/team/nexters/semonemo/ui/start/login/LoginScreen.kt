04ab46f2896373d03041ec52ffaaacc21ed28cf4
DESKTOP-SGS6ULA\evergreen <tkdfhr7497@naver.com>
2022-07-28 21:58:29 +0900

    refactor : 형식 변경

--- app/src/main/java/team/nexters/semonemo/ui/start/login/LoginScreen.kt
+++ app/src/main/java/team/nexters/semonemo/ui/start/login/LoginScreen.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -100,6 +101,7 @@ private fun LoginScreen(
                 .fillMaxWidth()
                 .height(48.dp),
             onClick = { onLogin(code) },
+            shape = RoundedCornerShape(8.dp),
             text = stringResource(id = R.string.start)
         )
         Text(
