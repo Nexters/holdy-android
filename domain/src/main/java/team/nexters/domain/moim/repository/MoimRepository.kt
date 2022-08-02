621fd5b5f24eb1d90ab5fd3fdc8bcbed942620b2
DESKTOP-SGS6ULA\evergreen <tkdfhr7497@naver.com>
2022-08-03 00:22:56 +0900

    feat : domain layer 작성

--- domain/src/main/java/team/nexters/domain/moim/repository/MoimRepository.kt
+++ domain/src/main/java/team/nexters/domain/moim/repository/MoimRepository.kt
@@ -1,7 +1,10 @@
 package team.nexters.domain.moim.repository
 
+import team.nexters.domain.moim.model.MoimCreateModel
 import team.nexters.domain.moim.model.MoimModel
+import team.nexters.domain.moim.model.MoimResponseModel
 
 interface MoimRepository {
     suspend fun fetchMoimList(): List<MoimModel>
+    suspend fun createMoim(moimCreateModel: MoimCreateModel): MoimResponseModel
 }
