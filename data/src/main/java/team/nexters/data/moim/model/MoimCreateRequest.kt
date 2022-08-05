95a4314f0b8ba7524309f6c042daf517e5dc7d84
DESKTOP-SGS6ULA\evergreen <tkdfhr7497@naver.com>
2022-08-05 22:10:04 +0900

    feat : data request 직렬화

--- data/src/main/java/team/nexters/data/moim/model/MoimCreateRequest.kt
+++ data/src/main/java/team/nexters/data/moim/model/MoimCreateRequest.kt
@@ -1,13 +1,21 @@
 package team.nexters.data.moim.model
 
+import com.google.gson.annotations.SerializedName
+
 data class MoimCreateRequest(
+    @SerializedName("startDate")
     val startDate: String,
+    @SerializedName("endDate")
     val endDate: String,
+    @SerializedName("place")
     val place: Place
-){
+) {
     data class Place(
+        @SerializedName("summary")
         val summary: String,
+        @SerializedName("address")
         val address: String,
+        @SerializedName("mapLink")
         val mapLink: String
     )
 }
