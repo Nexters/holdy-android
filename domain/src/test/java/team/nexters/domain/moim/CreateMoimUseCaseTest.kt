f3f19a4547e596b795d75d9ce904890eb94fa6dc
DESKTOP-SGS6ULA\evergreen <tkdfhr7497@naver.com>
2022-08-05 21:43:38 +0900

    test : usecase 테스트 추가

--- domain/src/test/java/team/nexters/domain/moim/CreateMoimUseCaseTest.kt
+++ domain/src/test/java/team/nexters/domain/moim/CreateMoimUseCaseTest.kt
@@ -1,4 +1,46 @@
 package team.nexters.domain.moim
 
+import io.mockk.coEvery
+import io.mockk.impl.annotations.MockK
+import io.mockk.junit5.MockKExtension
+import kotlinx.coroutines.test.runTest
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import team.nexters.domain.moim.model.MoimCreateModel
+import team.nexters.domain.moim.model.MoimResponseModel
+import team.nexters.domain.moim.usecase.CreateMoimUseCase
+
+@ExtendWith(MockKExtension::class)
 class CreateMoimUseCaseTest {
+
+    @MockK
+    private lateinit var createMoimUseCase: CreateMoimUseCase
+
+    private val moimCreateModel = MoimCreateModel(
+        "2022-07-29T15:00:00",
+        "2022-07-29T18:00:00",
+        MoimCreateModel.Place(
+            "상록이집",
+            "상록이집주소",
+            "안알랴줌"
+        )
+    )
+
+    @BeforeEach
+    fun init() {
+        coEvery {
+            createMoimUseCase(
+                moimCreateModel
+            )
+        } returns Result.success(MoimResponseModel("성공", 101))
+    }
+
+    @DisplayName("test success")
+    @Test
+    fun `Usecase return MoimResponseModel`() = runTest {
+        val result = createMoimUseCase(moimCreateModel)
+        assert(result.isSuccess)
+    }
 }
