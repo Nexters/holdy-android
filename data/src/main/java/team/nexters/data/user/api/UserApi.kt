b8c9d1fbdbf51b5ef51aa067af48e0460832dc6d
DESKTOP-SGS6ULA\evergreen <tkdfhr7497@naver.com>
2022-08-09 20:04:50 +0900

    API return 형식 변경

--- data/src/main/java/team/nexters/data/user/api/UserApi.kt
+++ data/src/main/java/team/nexters/data/user/api/UserApi.kt
@@ -3,12 +3,13 @@ package team.nexters.data.user.api
 import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.POST
+import team.nexters.data.base.BaseResponse
 import team.nexters.data.user.model.request.LoginRequest
 import team.nexters.data.user.model.response.LoginResponse
 
 interface UserApi {
     @POST("/api/login")
     suspend fun login(
-        @Body loginRequestModel: LoginRequest
-    ): Response<LoginResponse>
+        @Body loginRequest: LoginRequest
+    ): Response<BaseResponse<LoginResponse>>
 }
