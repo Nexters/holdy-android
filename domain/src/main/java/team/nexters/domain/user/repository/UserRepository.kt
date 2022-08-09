3b0059f9f06a5f377277c6d87e67623b71ffc43e
DESKTOP-SGS6ULA\evergreen <tkdfhr7497@naver.com>
2022-08-09 20:03:07 +0900

    domain 레포지토리 변경

--- domain/src/main/java/team/nexters/domain/user/repository/UserRepository.kt
+++ domain/src/main/java/team/nexters/domain/user/repository/UserRepository.kt
@@ -1,8 +1,8 @@
 package team.nexters.domain.user.repository
 
 import team.nexters.domain.user.model.LoginModel
-import team.nexters.domain.user.model.LoginRequestModel
+import team.nexters.domain.user.usecase.LoginUseCase
 
 interface UserRepository {
-    suspend fun login(loginRequestModel: LoginRequestModel): LoginModel
+    suspend fun login(loginRequestModel: LoginUseCase.Param): LoginModel
 }
