f09fdc598997671f802a98769b07cedc2ff3f006
DESKTOP-SGS6ULA\evergreen <tkdfhr7497@naver.com>
2022-08-09 20:04:08 +0900

    repository 구현체 수정

--- data/src/main/java/team/nexters/data/moim/repository/MoimRepositoryImpl.kt
+++ data/src/main/java/team/nexters/data/moim/repository/MoimRepositoryImpl.kt
@@ -1,32 +1,24 @@
 package team.nexters.data.moim.repository
 
-import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.withContext
-import team.nexters.shared.di.IoDispatcher
 import team.nexters.data.extension.apiCall
 import team.nexters.data.moim.api.MoimApi
 import team.nexters.data.moim.mapper.toData
 import team.nexters.data.moim.mapper.toDomain
-import team.nexters.domain.moim.model.MoimCreateModel
 import team.nexters.domain.moim.model.MoimModel
 import team.nexters.domain.moim.model.MoimResponseModel
 import team.nexters.domain.moim.repository.MoimRepository
+import team.nexters.domain.moim.usecase.CreateMoimUseCase
 import javax.inject.Inject
 
 class MoimRepositoryImpl @Inject constructor(
     private val moimApi: MoimApi,
-    @IoDispatcher private val dispatcher: CoroutineDispatcher
 ) : MoimRepository {
     override suspend fun fetchMoimList(): List<MoimModel> =
-        withContext(dispatcher) {
-            moimApi.fetchMoimList().apiCall().map {
-                it.toDomain()
-            }.toList()
-        }
+        moimApi.fetchMoimList().apiCall().map {
+            it.toDomain()
+        }.toList()
 
-    override suspend fun createMoim(moimCreateModel: MoimCreateModel): MoimResponseModel =
-        withContext(dispatcher) {
-            moimApi.createMoim(moimCreateModel.toData()).apiCall().toDomain()
-        }
+    override suspend fun createMoim(moimCreateModel: CreateMoimUseCase.Param): MoimResponseModel =
+        moimApi.createMoim(moimCreateModel.toData()).apiCall().toDomain()
 
 }
--- data/src/main/java/team/nexters/data/user/repository/UserRepositoryImpl.kt
+++ data/src/main/java/team/nexters/data/user/repository/UserRepositoryImpl.kt
@@ -1,29 +1,24 @@
 package team.nexters.data.user.repository
 
-import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.withContext
 import team.nexters.data.datastore.DataStoreManager
-import team.nexters.shared.di.IoDispatcher
 import team.nexters.data.extension.apiCall
 import team.nexters.data.extension.headerIntercept
 import team.nexters.data.user.api.UserApi
 import team.nexters.data.user.mapper.toData
 import team.nexters.data.user.mapper.toDomain
 import team.nexters.domain.user.model.LoginModel
-import team.nexters.domain.user.model.LoginRequestModel
 import team.nexters.domain.user.repository.UserRepository
+import team.nexters.domain.user.usecase.LoginUseCase
 import javax.inject.Inject
 
 class UserRepositoryImpl @Inject constructor(
     private val userApi: UserApi,
-    @IoDispatcher private val dispatcher: CoroutineDispatcher,
     private val dataStore: DataStoreManager
 ) : UserRepository {
-    override suspend fun login(loginRequestModel: LoginRequestModel): LoginModel =
-        withContext(dispatcher) {
-            userApi.login(loginRequestModel.toData())
-                .headerIntercept(dataStore)
-                .apiCall()
-                .toDomain()
-        }
+    override suspend fun login(loginRequestModel: LoginUseCase.Param): LoginModel =
+        userApi.login(loginRequestModel.toData())
+            .headerIntercept(dataStore)
+            .apiCall()
+            .toDomain()
+
 }
