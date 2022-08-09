5b23af9cd8dce8f740a3bf6b442efd8dec5a33fa
DESKTOP-SGS6ULA\evergreen <tkdfhr7497@naver.com>
2022-08-09 20:03:55 +0900

    usecase param 변경

--- domain/src/main/java/team/nexters/domain/moim/usecase/CreateMoimUseCase.kt
+++ domain/src/main/java/team/nexters/domain/moim/usecase/CreateMoimUseCase.kt
@@ -1,13 +1,28 @@
 package team.nexters.domain.moim.usecase
 
-import team.nexters.domain.moim.model.MoimCreateModel
+import kotlinx.coroutines.CoroutineDispatcher
+import team.nexters.domain.CoroutineUseCase
+import team.nexters.domain.moim.model.MoimResponseModel
 import team.nexters.domain.moim.repository.MoimRepository
+import team.nexters.shared.di.IoDispatcher
 import javax.inject.Inject
 
 class CreateMoimUseCase @Inject constructor(
-    private val repository: MoimRepository
-) {
-    suspend operator fun invoke(moimCreateModel: MoimCreateModel) = runCatching {
-        repository.createMoim(moimCreateModel)
-    }
+    private val repository: MoimRepository,
+    @IoDispatcher private val dispatcher: CoroutineDispatcher
+) : CoroutineUseCase<CreateMoimUseCase.Param, MoimResponseModel>(dispatcher) {
+
+    override suspend fun execute(param: Param): MoimResponseModel = repository.createMoim(param)
+
+    data class Param(
+        val startDate: String,
+        val endDate: String,
+        val place: Place
+    )
+
+    data class Place(
+        val summary: String,
+        val address: String,
+        val mapLink: String
+    )
 }
--- domain/src/main/java/team/nexters/domain/moim/usecase/FetchMoimListUseCase.kt
+++ domain/src/main/java/team/nexters/domain/moim/usecase/FetchMoimListUseCase.kt
@@ -1,12 +1,18 @@
 package team.nexters.domain.moim.usecase
 
+import kotlinx.coroutines.CoroutineDispatcher
+import team.nexters.domain.CoroutineUseCase
+import team.nexters.domain.moim.model.MoimModel
 import team.nexters.domain.moim.repository.MoimRepository
+import team.nexters.shared.di.IoDispatcher
 import javax.inject.Inject
 
 class FetchMoimListUseCase @Inject constructor(
-    private val repository: MoimRepository
-) {
-    suspend operator fun invoke() = runCatching {
+    private val repository: MoimRepository,
+    @IoDispatcher private val dispatcher: CoroutineDispatcher
+) : CoroutineUseCase<Unit, List<MoimModel>>(dispatcher) {
+
+    override suspend fun execute(param: Unit): List<MoimModel> =
         repository.fetchMoimList()
-    }
+
 }
