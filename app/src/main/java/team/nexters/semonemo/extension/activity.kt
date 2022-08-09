5e88851c08d2d99a63d883394fd7ced5355a5138
DESKTOP-SGS6ULA\evergreen <tkdfhr7497@naver.com>
2022-08-09 19:51:21 +0900

    activity 확장함수 제거

--- app/src/main/java/team/nexters/semonemo/extension/activity.kt
+++
@@ -1,7 +0,0 @@
-package team.nexters.semonemo.extension
-
-import android.app.Activity
-import android.widget.Toast
-
-fun Activity.basicExceptionHandler(message: String) =
-    Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
