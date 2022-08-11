487dd73c556b6afd928a83045f22fa2692da2fb0
DESKTOP-SGS6ULA\evergreen <tkdfhr7497@naver.com>
2022-08-11 16:39:30 +0900

    data단 형식 변경

--- data/src/main/java/team/nexters/data/moim/repository/MoimRepositoryImpl.kt
+++ data/src/main/java/team/nexters/data/moim/repository/MoimRepositoryImpl.kt
@@ -1,24 +1,31 @@
 package team.nexters.data.moim.repository
 
-import team.nexters.data.extension.apiCall
 import team.nexters.data.moim.api.MoimApi
 import team.nexters.data.moim.mapper.toData
 import team.nexters.data.moim.mapper.toDomain
+import team.nexters.data.util.ResponseHandler.handleApi
 import team.nexters.domain.moim.model.MoimModel
 import team.nexters.domain.moim.model.MoimResponseModel
 import team.nexters.domain.moim.repository.MoimRepository
 import team.nexters.domain.moim.usecase.CreateMoimUseCase
+import team.nexters.shared.ResultWrapper
+import team.nexters.shared.flatMap
 import javax.inject.Inject
 
 class MoimRepositoryImpl @Inject constructor(
     private val moimApi: MoimApi,
 ) : MoimRepository {
-    override suspend fun fetchMoimList(): List<MoimModel> =
-        moimApi.fetchMoimList().apiCall().map {
-            it.toDomain()
+    override suspend fun fetchMoimList(): ResultWrapper<List<MoimModel>> = handleApi {
+        moimApi.fetchMoimList()
+    }.flatMap {
+        it.map { moimResponse ->
+            moimResponse.toDomain()
         }.toList()
+    }
 
-    override suspend fun createMoim(moimCreateModel: CreateMoimUseCase.Param): MoimResponseModel =
-        moimApi.createMoim(moimCreateModel.toData()).apiCall().toDomain()
+    override suspend fun createMoim(moimCreateModel: CreateMoimUseCase.Param): ResultWrapper<MoimResponseModel> =
+        handleApi {
+            moimApi.createMoim(moimCreateModel.toData())
+        }.flatMap { it.toDomain() }
 
 }
