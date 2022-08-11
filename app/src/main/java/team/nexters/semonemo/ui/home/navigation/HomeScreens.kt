1af0d82dec0f9c7658afceb4adae91f6e07a400a
DESKTOP-SGS6ULA\evergreen <tkdfhr7497@naver.com>
2022-08-12 01:31:27 +0900

    Hold 화면 추가

--- app/src/main/java/team/nexters/semonemo/ui/home/navigation/HomeScreens.kt
+++ app/src/main/java/team/nexters/semonemo/ui/home/navigation/HomeScreens.kt
@@ -1,13 +1,10 @@
 package team.nexters.semonemo.ui.home.navigation
 
-import team.nexters.semonemo.R
-
 sealed class HomeScreens(
     val route: String,
-    val title: Int,
-    val icon: Int
 ) {
-    object List : HomeScreens("LIST", R.string.description_home, R.drawable.ic_launcher_background)
-    object Creating : HomeScreens("CREATING", R.string.description_creating, R.drawable.ic_launcher_background)
-    object Reward : HomeScreens("REWARD", R.string.description_reward, R.drawable.ic_launcher_background)
+    object List : HomeScreens("LIST")
+    object Creating : HomeScreens("CREATING")
+    object Reward : HomeScreens("REWARD")
+    object Hold: HomeScreens("HOLD")
 }
