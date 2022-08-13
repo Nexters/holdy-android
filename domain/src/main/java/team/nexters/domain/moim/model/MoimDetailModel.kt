13f9651183e5cd7cd9cfc855ba5c9ff37e6b9290
DESKTOP-UDRA8DA\tkdfh <tkdfhr7497@naver.com>
2022-08-13 19:32:18 +0900

    모임상세 domain

---
+++ domain/src/main/java/team/nexters/domain/moim/model/MoimDetailModel.kt
@@ -0,0 +1,24 @@
+package team.nexters.domain.moim.model
+
+data class MoimDetailModel(
+    val id: Int,
+    val startDate: String,
+    val endDate: String,
+    val isEnd: Boolean,
+    val place: Place,
+    val host: Host,
+    val loginUser: LoginUser,
+    val participants: List<Participant>
+)
+
+data class LoginUser(
+    val isHost: Boolean,
+    val wantToAttend: Boolean
+)
+
+data class Participant(
+    val id: Int,
+    val nickname: String,
+    val group: String,
+    val attend: Boolean
+)
--- domain/src/main/java/team/nexters/domain/moim/repository/MoimRepository.kt
+++ domain/src/main/java/team/nexters/domain/moim/repository/MoimRepository.kt
@@ -1,11 +1,14 @@
 package team.nexters.domain.moim.repository
 
+import team.nexters.domain.moim.model.MoimDetailModel
 import team.nexters.domain.moim.model.MoimModel
 import team.nexters.domain.moim.model.MoimResponseModel
 import team.nexters.domain.moim.usecase.CreateMoimUseCase
+import team.nexters.domain.moim.usecase.GetMoimDetailUseCase
 import team.nexters.shared.ResultWrapper
 
 interface MoimRepository {
     suspend fun fetchMoimList(): ResultWrapper<List<MoimModel>>
     suspend fun createMoim(param: CreateMoimUseCase.Param): ResultWrapper<MoimResponseModel>
+    suspend fun getMoimDetail(param: GetMoimDetailUseCase.Param): ResultWrapper<MoimDetailModel>
 }
