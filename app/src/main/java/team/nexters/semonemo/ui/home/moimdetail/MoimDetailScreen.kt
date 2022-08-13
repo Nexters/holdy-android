90450bf7f88d6c73a2699ec3d28cc3444d03e643
Deo <nexters@kakao.com>
2022-08-13 20:52:56 +0900

    refactor(kakao-share): 카톡으로 모임 초대하기 로직 상단으로 이동

--- app/src/main/java/team/nexters/semonemo/ui/home/moimdetail/MoimDetailScreen.kt
+++ app/src/main/java/team/nexters/semonemo/ui/home/moimdetail/MoimDetailScreen.kt
@@ -1,5 +1,6 @@
 package team.nexters.semonemo.ui.home.moimdetail
 
+import android.content.Context
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -21,10 +22,15 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import androidx.core.content.ContextCompat
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
+import com.kakao.sdk.common.util.KakaoCustomTabsClient
+import com.kakao.sdk.share.ShareClient
+import com.kakao.sdk.share.WebSharerClient
 import team.nexters.semonemo.R
 import team.nexters.semonemo.common.Button
 import team.nexters.semonemo.theme.Primary
@@ -32,6 +38,7 @@ import team.nexters.semonemo.ui.home.model.MoimInfo
 import team.nexters.semonemo.ui.home.model.MoimInfoDummy
 import team.nexters.semonemo.ui.home.moimdetail.component.InformationContent
 import team.nexters.semonemo.ui.home.moimdetail.component.ParticipantContent
+import timber.log.Timber
 
 @Composable
 internal fun MoimDetailScreen(
@@ -53,6 +60,7 @@ private fun MoimDetailScreen(
     onBackPressed: () -> Unit = {},
     moimInfo: MoimInfo = MoimInfoDummy
 ) {
+    val context = LocalContext.current
     val scaffoldState = rememberBackdropScaffoldState(BackdropValue.Revealed)
     val isHostMode by remember { mutableStateOf(true) } // 호스트 판단 추후 변경
     var isCome by remember { mutableStateOf(false) } // 갈게요 Button
@@ -65,7 +73,8 @@ private fun MoimDetailScreen(
                     .padding(horizontal = 24.dp)
                     .padding(top = 32.dp),
                 isHostMode = isHostMode,
-                scaffoldState
+                scaffoldState,
+                onInvite = { kakaoShare(context) },
             )
         },
         frontLayerScrimColor = Color.Unspecified,
@@ -119,3 +128,41 @@ private fun MoimDetailScreen(
     }
 }
 
+
+// kakaoShare 카카오 공유하기
+// https://developers.kakao.com/docs/latest/ko/message/android-link#custom-template-msg
+private fun kakaoShare(
+    context: Context,
+    templateId: Long = 81345,
+    templateArgs: Map<String, String>? = null
+) {
+    if (ShareClient.instance.isKakaoTalkSharingAvailable(context)) {
+        // 카카오톡으로 카카오톡 공유 가능
+        ShareClient.instance.shareCustom(context, templateId, templateArgs) { sharingResult, error ->
+            if (error != null) {
+                Timber.e("카카오톡 공유 실패", error)
+            }
+            else if (sharingResult != null) {
+                Timber.d("카카오톡 공유 성공 ${sharingResult.intent}")
+                ContextCompat.startActivity(context, sharingResult.intent, null)
+
+                // 카카오톡 공유에 성공했지만 아래 경고 메시지가 존재할 경우 일부 컨텐츠가 정상 동작하지 않을 수 있습니다.
+                Timber.w("Warning Msg: ${sharingResult.warningMsg}")
+                Timber.w("Argument Msg: ${sharingResult.argumentMsg}")
+            }
+        }
+    }
+    else {
+        // 카카오톡 미설치: 웹 공유 사용 권장
+        // 웹 공유 예시 코드
+        val sharerUrl = WebSharerClient.instance.makeCustomUrl(templateId)
+
+        // CustomTabsServiceConnection 지원 브라우저 열기
+        // ex) Chrome, 삼성 인터넷, FireFox, 웨일 등
+        try {
+            KakaoCustomTabsClient.openWithDefault(context, sharerUrl)
+        } catch(e: UnsupportedOperationException) {
+            // CustomTabsServiceConnection 지원 브라우저가 없을 때 예외처리
+        }
+    }
+}
