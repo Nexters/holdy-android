fa24fce18e43b7fac4b815894f193162129144e8
DESKTOP-SGS6ULA\evergreen <tkdfhr7497@naver.com>
2022-08-15 13:17:53 +0900

    스플래시 화면 UI

--- app/src/main/java/team/nexters/semonemo/ui/splash/SplashActivity.kt
+++ app/src/main/java/team/nexters/semonemo/ui/splash/SplashActivity.kt
@@ -5,13 +5,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import com.google.firebase.dynamiclinks.PendingDynamicLinkData
 import com.google.firebase.dynamiclinks.ktx.dynamicLinks
@@ -19,6 +25,7 @@ import com.google.firebase.ktx.Firebase
 import dagger.hilt.android.AndroidEntryPoint
 import team.nexters.semonemo.BuildConfig
 import team.nexters.semonemo.R
+import team.nexters.semonemo.theme.Primary
 import team.nexters.semonemo.theme.SemoNemoTheme
 import team.nexters.semonemo.theme.Tertiary
 import team.nexters.semonemo.ui.home.HomeActivity
@@ -112,15 +119,27 @@ fun SplashScreen() {
     val systemUiController = rememberSystemUiController()
     SideEffect {
         systemUiController.setSystemBarsColor(
-            color = Tertiary
+            color = Primary
         )
     }
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(Tertiary),
-        horizontalAlignment = Alignment.CenterHorizontally,
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = Primary
     ) {
+        Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.holdy_logo),
+                contentDescription = stringResource(R.string.holdy_logo)
+            )
+        }
+        Image(
+            modifier = Modifier.fillMaxSize(),
+            painter = painterResource(id = R.drawable.splash),
+            contentDescription = stringResource(R.string.splash)
+        )
     }
 }
 
