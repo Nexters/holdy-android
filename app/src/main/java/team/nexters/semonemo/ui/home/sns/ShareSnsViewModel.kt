5fe2b22884ccca0e327e0a66455be46da7285a2a
DESKTOP-SGS6ULA\evergreen <tkdfhr7497@naver.com>
2022-08-18 01:24:07 +0900

    viewmodel 코드

--- app/src/main/java/team/nexters/semonemo/ui/home/sns/ShareSnsViewModel.kt
+++ app/src/main/java/team/nexters/semonemo/ui/home/sns/ShareSnsViewModel.kt
@@ -1,4 +1,19 @@
 package team.nexters.semonemo.ui.home.sns
 
-class ShareSnsViewModel {
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.launch
+import team.nexters.semonemo.base.BaseViewModel
+
+class ShareSnsViewModel : BaseViewModel() {
+
+    private val _eventFlow = MutableSharedFlow<ShareSnsEvent>(extraBufferCapacity = 1)
+    val eventFlow = _eventFlow.asSharedFlow()
+
+    fun postEvent(event: ShareSnsEvent) {
+        viewModelScope.launch {
+            _eventFlow.emit(event)
+        }
+    }
 }
