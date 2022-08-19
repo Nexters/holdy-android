2e330f0a31202c44b456739921c3c8e025a472ef
DESKTOP-SGS6ULA\evergreen <tkdfhr7497@naver.com>
2022-08-19 18:11:52 +0900

    app version 1.0.0

--- buildSrc/src/main/kotlin/Apps.kt
+++ buildSrc/src/main/kotlin/Apps.kt
@@ -6,7 +6,7 @@ object Apps {
     const val compileSdk = 32
     const val jvmTarget = "11"
     const val versionCode = 1
-    const val versionName = "1.0"
+    const val versionName = "1.0.0"
     val targetCompat = JavaVersion.VERSION_11
     val sourceCompat = JavaVersion.VERSION_11
 }
