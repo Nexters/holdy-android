c0dad58d4c0854d82c0368280c7a75f1b2c2f94d
DESKTOP-SGS6ULA\evergreen <tkdfhr7497@naver.com>
2022-08-19 18:09:23 +0900

    새로운 스탬프 domain layer

--- domain/src/main/java/team/nexters/domain/hold/repository/HoldRepository.kt
+++ domain/src/main/java/team/nexters/domain/hold/repository/HoldRepository.kt
@@ -7,4 +7,5 @@ import team.nexters.shared.ResultWrapper
 interface HoldRepository {
     suspend fun getHoldList(): ResultWrapper<List<Hold>>
     suspend fun getNewHoldList(): ResultWrapper<List<NewHold>>
+    suspend fun getRemovedNewHoldList(): ResultWrapper<List<NewHold>>
 }
