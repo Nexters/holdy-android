348b821b6f00b9b92c4edc0dea5f356aba01162b
EvergreenTree97 <tkdfhr7497@naver.com>
2022-08-30 21:22:39 +0900

    version 1.0.1

--- buildSrc/src/main/kotlin/Apps.kt
+++ buildSrc/src/main/kotlin/Apps.kt
@@ -5,8 +5,8 @@ object Apps {
     const val targetSdk = 32
     const val compileSdk = 32
     const val jvmTarget = "11"
-    const val versionCode = 1
-    const val versionName = "1.0.0"
+    const val versionCode = 2
+    const val versionName = "1.0.1"
     val targetCompat = JavaVersion.VERSION_11
     val sourceCompat = JavaVersion.VERSION_11
 }
